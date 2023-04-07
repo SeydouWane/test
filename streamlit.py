2612cbac19b7c60b2175c211af1c24da5c38133a
SeydouWane <129470066+SeydouWane@users.noreply.github.com>
2023-04-07 04:47:14 +0000

    .pyplot

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 # Ajouter les modules nécessaires pour la création du fichier HTML
 from io import StringIO

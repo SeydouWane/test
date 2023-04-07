4bd3fcebc9cc1da4f92336c46350f404f446c73c
SeydouWane <129470066+SeydouWane@users.noreply.github.com>
2023-04-07 04:57:30 +0000

    retour

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 # Ajouter les modules nécessaires pour la création du fichier HTML
